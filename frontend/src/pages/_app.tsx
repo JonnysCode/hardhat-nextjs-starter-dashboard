bb0f3ab95cea9427abdd1f769753c2ee0ecd93e9
Jonathan Gruss <jonathangruss@hotmail.com>
2022-08-08 14:44:18 +0200

    use yarn

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,17 +4,12 @@ import React, { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import '@rainbow-me/rainbowkit/styles.css'
 import { ThemeProvider } from 'next-themes'
-import { themeChange } from 'theme-change'
 
 import Web3Wrapper from '@/components/Layout/Web3Wrapper'
 
 function MyApp({ Component, pageProps }: AppProps) {
   // suppress useLayoutEffect warnings when running outside a browser
   if (!typeof window) React.useLayoutEffect = useEffect
-
-  useEffect(() => {
-    themeChange(false)
-  }, [])
 
   return (
     <ThemeProvider themes={['light', 'dark', 'cupcake', 'lofi', 'cyberpunk']}>
